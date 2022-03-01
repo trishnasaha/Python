49b2a5091cb6e27916a5d7b58b72cd394ff2430d
trishnasaha <trishna.saha27@gmail.com>
2022-03-01 13:15:07 +0200

    created

---
+++ PY4E/score_range.py
@@ -0,0 +1,16 @@
+sc = input("score:")
+score = float(sc)
+if score < 0.0 or score > 1.0:
+    print("out of range")
+
+elif score >= 0.9:
+    print("grade A")
+elif score >= 0.8:
+    print("grade B")
+elif score >= 0.7:
+    print("grade C")
+elif score >= 0.6:
+    print("grade D")
+elif score < 0.6:
+    print("garde F")
+   
