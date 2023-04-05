9461e9250ddfd0a65f23beaaf4668ef3f2d29862
trishnasaha <trishna.saha27@gmail.com>
2023-04-05 13:51:00 -0400

    update

--- PY4E/pay.py
+++ PY4E/pay.py
@@ -1,5 +1,7 @@
+## Write a program to prompt the user for hours and rate per hour using input to compute gross pay. Use 35 hours and a rate of 2.75 per hour to test the program (the pay should be 96.25).You should use input to read a string and float() to convert the string to a number. Do not worry about error checking or bad user data.
+
 # calculate pay
-xh = input ("hours: ")
+hrs = input ("enter hours: ")
 xr = input ("rate: ")
-xp = float(xh) * float(xr)
-print ("pay:",xp)
+xp = float(hrs) * float(xr)
+print ("Pay:",xp)
